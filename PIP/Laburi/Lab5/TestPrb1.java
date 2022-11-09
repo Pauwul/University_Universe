7ec576af18a2afad2d36311b2b524e7f1aaa4a68
Pauwul's macbook <petrila.paulcosmin@gmail.com>
2022-11-09 09:08:59 +0200

    mac dump

---
+++ PIP/Laburi/Lab5/TestPrb1.java
@@ -0,0 +1,7 @@
+public class TestPrb1 {
+    public static void main(String[] args) {
+        String msg = "-+u+s+c-+e+r+t-+a-+i-+s+t+r-+y+u-+l+e+s";
+        char result = msg.charAt(0);
+        System.out.println(result);
+    }
+}
---
+++ PIP/README.md
@@ -0,0 +1,5 @@
+# Platform Independent Programming
+
+## Abstract
+
+## 
